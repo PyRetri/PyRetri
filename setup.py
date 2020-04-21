47719f1137b1cdb7168527698c11fbb35b2e02b6
hubenyi <867320491@qq.com>
2020-04-21 23:03:25 +0800

    upload

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
         ],
         license='Apache License 2.0',
         install_requires=[
-            'numpy', 'torch>=1.1', 'torchvision>=0.4', 'sklearn', 'yacs', 'tqdm',
+            'numpy', 'torch>=1.2', 'torchvision>=0.4', 'sklearn', 'yacs', 'tqdm',
         ],
         packages=find_packages(),
         zip_safe=False)
