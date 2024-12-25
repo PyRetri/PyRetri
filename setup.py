c1a614c8ff0d5229e78c79810ac42ca36f836e5b
thomasflittner <thf1257@thi.de>
2024-12-25 13:08:37 +0900

    fix

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
         ],
         license='Apache License 2.0',
         install_requires=[
-            'numpy', 'torch>=2.2.2', 'torchvision==0.18.1', 'scikit-learn', 'yacs', 'tqdm',
+            'numpy', 'torch>=2.2.2', 'torchvision>=0.17.2', 'scikit-learn', 'yacs', 'tqdm',
         ],
         packages=find_packages(),
         zip_safe=False)
